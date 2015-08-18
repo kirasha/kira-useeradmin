06e30d3ebe10a69e6a35ee8084c15626ce093d7e
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-08-17 23:56:15 -0400

    Added unit tests for user model

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -5,14 +5,7 @@ var should    = require('should'),
     config    = require('../../config/environment'),
     User      = require('./user.model');
 
-var user = new User({
-  provider: 'local',
-  firstName: 'Fake User',
-  lastName: 'Test',
-  username: 'test',
-  email: 'tes@ttest.com',
-  password: 'password123'
-});
+var user;
 
 describe('User Model', function () {
   before(function (done) {
@@ -28,6 +21,18 @@ describe('User Model', function () {
     mongoose.connection.close(done);
   });
 
+  beforeEach(function (done) {
+    user = new User({
+      provider: 'local',
+      firstName: 'Fake User',
+      lastName: 'Test',
+      username: 'test',
+      email: 'tes@ttest.com',
+      password: 'password123'
+    });
+    done();
+  });
+
   afterEach(function (done) {
     User.remove().exec().then(function () {
       done();
@@ -41,8 +46,23 @@ describe('User Model', function () {
     });
   });
 
+  it('should save a user', function (done) {
+    user.save(function (err, user) {
+      should.not.exist(err);
+      should.exist(user);
+      done();
+    });
+  });
+
+  it('it should have a virtual method fullName', function (done) {
+    user.save(function (err, res) {
+      res.fullName.should.equal(user.firstName + ' ' + user.lastName);
+      done();
+    });
+  });
+
   it('should fail when saving a duplicate user', function (done) {
-    user.save(function () {
+    user.save(function (err) {
       var userDup = new User(user);
       userDup.save(function (err) {
         should.exist(err);
@@ -59,11 +79,42 @@ describe('User Model', function () {
     });
   });
 
-  it('should authenticate user if password is valid', function () {
-    return user.authenticate('password').should.be.true;
+  it('should fail when saving with a password less than 6 characters', function (done) {
+    user.password = 'abc';
+    user.save(function (err) {
+      should.exist(err);
+      done();
+    });
   });
 
-  it('should not authenticate user if password is invalid', function () {
-    return user.authenticate('blah').should.not.be.true;
+  it('should authenticate user if password is valid', function (done) {
+    user.save(function (err, res) {
+      should.not.exist(err);
+      should.exist(res);
+      res.authenticate('password123').should.be.true();
+      done();
+    });
+  });
+
+  it('should not authenticate user if password is invalid', function (done) {
+    user.save(function (err, res) {
+      should.not.exist(err);
+      should.exist(res);
+      res.authenticate('fake123').should.not.be.true();
+      done();
+    });
+  });
+
+  it('should find unique username', function (done) {
+    user.save(function (err, res) {
+      should.exist(res);
+      User.findUniqueUsername(res.username, '', function (username) {
+        should.exist(username);
+        username.should.not.be.empty();
+        username.should.not.equal(res.username);
+        done();
+      });
+
+    });
   });
 });
