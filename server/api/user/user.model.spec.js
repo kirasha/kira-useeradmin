8f72ee568a4676dea99003995f8a9414296d0bdc
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-09-30 04:58:41 -0400

    Reorganised unit tests in describe sections

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -41,107 +41,115 @@ describe('User Model', function () {
     });
   });
 
-  it('should save a user', function (done) {
-    user.save(function (err, user) {
-      should.not.exist(err);
-      should.exist(user);
-      done();
-    });
-  });
-
-  it('should save a user and set a default role', function (done) {
-    var role = new Role({
-        name: 'tester',
-        description: 'Description of this role',
-        builtIn: true,
-        default: true,
-        active: true
-      });
-
-    role.save(function (err, savedRole) {
-      user.save(function (err, savedUser) {
+  describe('Saving user', function () {
+    it('should save a user', function (done) {
+      user.save(function (err, user) {
         should.not.exist(err);
-        should.exist(savedUser);
-        savedUser.should.have.property('role');
-        User.findOne({ _id: savedUser.id })
-            .populate('role')
-            .exec(function (err, userRole) {
-              should.not.exist(err);
-              should.exist(userRole);
-              userRole.should.have.property('role');
-              userRole.role.should.be.ok();
-              userRole.role.name.should.equal(role.name);
-              done();
-            });
+        should.exist(user);
+        done();
       });
     });
-  });
 
-  it('it should have a virtual method fullName', function (done) {
-    user.save(function (err, res) {
-      res.should.have.property('fullName');
-      should.not.exist(err);
-      res.fullName.should.equal(user.firstName + ' ' + user.lastName);
-      done();
+    it('should save a user and set a default role', function (done) {
+      var role = new Role({
+          name: 'tester',
+          description: 'Description of this role',
+          builtIn: true,
+          default: true,
+          active: true
+        });
+
+      role.save(function (err, savedRole) {
+        user.save(function (err, savedUser) {
+          should.not.exist(err);
+          should.exist(savedUser);
+          savedUser.should.have.property('role');
+          User.findOne({ _id: savedUser.id })
+              .populate('role')
+              .exec(function (err, userRole) {
+                should.not.exist(err);
+                should.exist(userRole);
+                userRole.should.have.property('role');
+                userRole.role.should.be.ok();
+                userRole.role.name.should.equal(role.name);
+                done();
+              });
+        });
+      });
     });
-  });
 
-  it('should fail when saving a duplicate user', function (done) {
-    user.save(function (err) {
-      should.not.exist(err);
-      var userDup = new User(user);
-      userDup.save(function (err) {
+    it('should fail when saving a duplicate user', function (done) {
+      user.save(function (err) {
+        should.not.exist(err);
+        var userDup = new User(user);
+        userDup.save(function (err) {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+
+    it('should fail when saving without an email', function (done) {
+      user.email = '';
+      user.save(function (err) {
         should.exist(err);
         done();
       });
     });
-  });
 
-  it('should fail when saving without an email', function (done) {
-    user.email = '';
-    user.save(function (err) {
-      should.exist(err);
-      done();
-    });
-  });
-
-  it('should fail when saving with a password less than 6 characters', function (done) {
-    user.password = 'abc';
-    user.save(function (err) {
-      should.exist(err);
-      done();
-    });
-  });
-
-  it('should authenticate user if password is valid', function (done) {
-    user.save(function (err, res) {
-      should.not.exist(err);
-      should.exist(res);
-      res.authenticate('password123').should.be.true();
-      done();
-    });
-  });
-
-  it('should not authenticate user if password is invalid', function (done) {
-    user.save(function (err, res) {
-      should.not.exist(err);
-      should.exist(res);
-      res.authenticate('fake123').should.not.be.true();
-      done();
-    });
-  });
-
-  it('should find unique username', function (done) {
-    user.save(function (err, res) {
-      should.exist(res);
-      should.not.exist(err);
-      User.findUniqueUsername(res.username, '', function (username) {
-        should.exist(username);
-        username.should.not.be.empty();
-        username.should.not.equal(res.username);
+    it('should fail when saving with a password less than 6 characters', function (done) {
+      user.password = 'abc';
+      user.save(function (err) {
+        should.exist(err);
         done();
       });
+    });
 
+  });
+
+  describe('User virtual properties', function () {
+    it('it should have a virtual method fullName', function (done) {
+      user.save(function (err, res) {
+        res.should.have.property('fullName');
+        should.not.exist(err);
+        res.fullName.should.equal(user.firstName + ' ' + user.lastName);
+        done();
+      });
+    });
+  });
+
+  describe('User model methods', function () {
+    it('should authenticate user if password is valid', function (done) {
+      user.save(function (err, res) {
+        should.not.exist(err);
+        should.exist(res);
+        res.authenticate('password123').should.be.true();
+        done();
+      });
+    });
+
+    it('should not authenticate user if password is invalid', function (done) {
+      user.save(function (err, res) {
+        should.not.exist(err);
+        should.exist(res);
+        res.authenticate('fake123').should.not.be.true();
+        done();
+      });
+    });
+  });
+
+  describe('User Model static methods', function () {
+    it('should find unique username', function (done) {
+      user.save(function (err, res) {
+        should.exist(res);
+        should.not.exist(err);
+        User.findUniqueUsername(res.username, '', function (username) {
+          should.exist(username);
+          username.should.not.be.empty();
+          username.should.not.equal(res.username);
+          done();
+        });
+      });
     });
   });
 });
